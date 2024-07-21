4dd2f2ee3e310734ee16a88665479159370c75e7
Ganga-tharank <154905189+Ganga-tharank@users.noreply.github.com>
2024-07-21 20:33:59 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import "bootstrap/dist/css/bootstrap.min.css";
 import Store from './redux/Store.jsx'
-import { Provider } from 'react-redux';
+import { Provider } from 'react-redux'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={Store}>
       <App />
     </Provider>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
